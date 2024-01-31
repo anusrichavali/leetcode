0a9dfc1d4a2b8bd684be1b3f06d889f9ef3df5e5
anusrichavali <64172984+anusrichavali@users.noreply.github.com>
2024-01-31 11:13:44 -0800

    Time: 1027 ms (5.01%) | Memory: 12.1 MB (81.92%) - LeetSync

--- 2714-left-and-right-sum-differences/left-and-right-sum-differences.py
+++ 2714-left-and-right-sum-differences/left-and-right-sum-differences.py
@@ -4,17 +4,14 @@ class Solution(object):
         :type nums: List[int]
         :rtype: List[int]
         """
-        leftSums = [0] * len(nums)
-        rightSums = [0] * len(nums)
         sums = [0] * len(nums)
         for i in range(len(nums)):
             for j in range(0, i):
-                leftSums[i] = leftSums[i] + nums[j]
+                sums[i] = sums[i] + nums[j]
             for k in range(i + 1, len(nums)):
-                rightSums[i] = rightSums[i] + nums[k]
+                sums[i] = sums[i] - nums[k]
+            sums[i] = abs(sums[i])
 
-        for i in range(len(nums)):
-            sums[i] = abs(leftSums[i] - rightSums[i])
         return sums
             
 
