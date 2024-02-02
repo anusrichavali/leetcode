e5b2317d9faf9d1c242b2850f1e291e6003e973e
anusrichavali <64172984+anusrichavali@users.noreply.github.com>
2024-02-02 15:46:39 -0800

    Time: 33 ms (46.47%) | Memory: 11.6 MB (86.54%) - LeetSync

---
+++ 2847-find-maximum-number-of-string-pairs/find-maximum-number-of-string-pairs.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def maximumNumberOfStringPairs(self, words):
+        """
+        :type words: List[str]
+        :rtype: int
+        """
+        rev = []
+        for i in words:
+            rev.append(i[::-1])
+        
+        ct = 0
+        print(rev)
+        for i in words:
+            if i in rev and words.index(i) != rev.index(i):
+                ct = ct + 1
+        return int(math.floor(ct/2))
+        
