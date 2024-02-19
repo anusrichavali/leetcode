19f1b4e19c256eb6fc51707badc6b70a67a619d7
anusrichavali <64172984+anusrichavali@users.noreply.github.com>
2024-02-18 19:14:39 -0800

    Time: 61 ms (15.36%) | Memory: 11.9 MB (99.29%) - LeetSync

---
+++ 242-valid-anagram/valid-anagram.py
@@ -0,0 +1,23 @@
+class Solution(object):
+    def isAnagram(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """
+        map = {}
+        if len(s) != len(t):
+            return False
+
+        for i in s:
+            map[i] = map.get(i, 0) + 1
+
+        for i in t:
+            if i not in map.keys():
+                return False
+            elif map[i] == 0:
+                return False
+            else:
+                map[i] = map.get(i) - 1
+
+        return True
