d22cf5dc75f83069c2e70fdcb8444c222b02b5fb
anusrichavali <64172984+anusrichavali@users.noreply.github.com>
2024-03-04 23:51:21 -0800

    Time: 25 ms (27.29%) | Memory: 11.7 MB (83.49%) - LeetSync

---
+++ 46-permutations/permutations.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def permute(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        answer = []
+        #base case
+        if len(nums) == 1:
+            return [nums[:]]
+
+        for i in range(len(nums)):
+            n = nums.pop(0)
+            perms = self.permute(nums)
+
+            for perm in perms:
+                perm.append(n)
+            answer.extend(perms)
+            nums.append(n)
+        
+        return answer
