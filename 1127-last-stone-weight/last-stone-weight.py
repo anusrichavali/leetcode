352001cd0dffa179909cd89d871dc10b4b509b76
anusrichavali <64172984+anusrichavali@users.noreply.github.com>
2024-03-08 22:22:32 -0800

    Time: 27 ms (5.64%) | Memory: 11.6 MB (67.77%) - LeetSync

---
+++ 1127-last-stone-weight/last-stone-weight.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def lastStoneWeight(self, stones):
+        """
+        :type stones: List[int]
+        :rtype: int
+        """
+        stones = [-i for i in stones]
+        heapq.heapify(stones)
+        while len(stones) > 1:
+            a = -(heapq.heappop(stones))
+            b = -(heapq.heappop(stones))
+            if b > a:
+                heapq.heappush(stones, a - b)
+            elif b < a:
+                heapq.heappush(stones, b - a)
+        if stones:
+            return -(heapq.heappop(stones))
+        else:
+            return 0
