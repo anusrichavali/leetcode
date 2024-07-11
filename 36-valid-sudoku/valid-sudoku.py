f5fefe998a4480002d1041b4f50c465309782eec
Anusri Chavali <64172984+anusrichavali@users.noreply.github.com>
2024-07-10 21:16:58 -0700

    Time: 93 ms (49.90%) | Memory: 16.5 MB (78.77%) - LeetSync

--- 36-valid-sudoku/valid-sudoku.py
+++ 36-valid-sudoku/valid-sudoku.py
@@ -1,27 +1,19 @@
-class Solution(object):
-    def isValidSudoku(self, board):
-        """
-        :type board: List[List[str]]
-        :rtype: bool
-        """
-        #part 1: iterating through rows
-        columns = collections.defaultdict(set)
-        rows = collections.defaultdict(set)
-        mini_grids = collections.defaultdict(set)
+class Solution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        rows = defaultdict(set)
+        cols = defaultdict(set)
+        squares = defaultdict(set)
+
         for r in range(9):
             for c in range(9):
-                if board[r][c] == '.':
+                if(board[r][c] == '.'):
                     continue
-                if board[r][c] in rows[r]:
-                    return False
-                if board[r][c] in columns[c]:
-                    return False
-                index_x = r/3
-                index_y = c/3
-                if board[r][c] in mini_grids[index_x, index_y]:
+                if(board[r][c] in rows[r] or board[r][c] in cols[c] or board[r][c] in squares[(r//3, c//3)]):
                     return False
                 rows[r].add(board[r][c])
-                columns[c].add(board[r][c])
-                mini_grids[(index_x, index_y)].add(board[r][c])
+                cols[c].add(board[r][c])
+                squares[(r//3, c//3)].add(board[r][c])
         return True
-        
+
+
+            
