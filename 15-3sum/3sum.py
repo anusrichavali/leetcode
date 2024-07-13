2180af9d7630ac5a92420889cb99b08febcf9212
Anusri Chavali <64172984+anusrichavali@users.noreply.github.com>
2024-07-13 14:52:20 -0700

    Time: 672 ms (67.35%) | Memory: 20.7 MB (62.99%) - LeetSync

--- 15-3sum/3sum.py
+++ 15-3sum/3sum.py
@@ -1,28 +1,27 @@
-class Solution(object):
-    def threeSum(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: List[List[int]]
-        """
-        result = []
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        res = []
         nums.sort()
-        
-        for index, item in enumerate(nums):
-            if index > 0 and item == nums[index - 1]:
-                continue
 
-            target = 0 - item
-            left = index + 1
-            right = len(nums) - 1
-            while left < right:
-                if (nums[left] + nums[right])  > target:
-                    right -= 1
-                elif (nums[left] + nums[right]) < target:
-                    left += 1
+        for i in range(len(nums)):
+            if i > 0 and nums[i] == nums[i-1]:
+                continue
+            
+            j = i + 1
+            k = len(nums) - 1
+
+            while j < k:
+                total = nums[i] + nums[j] + nums[k]
+
+                if total > 0:
+                    k -= 1
+                elif total < 0:
+                    j += 1
                 else:
-                    result.append([item, nums[left], nums[right]])
-                    left += 1
-                    while nums[left] == nums[left - 1] and left < right:
-                        left += 1
-        return result
+                    res.append([nums[i], nums[j], nums[k]])
+                    j += 1
+
+                    while nums[j] == nums[j-1] and j < k:
+                        j += 1
         
+        return res
