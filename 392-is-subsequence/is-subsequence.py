617095ec3cc494c1834a4a9a163ab6d062f87eb9
Anusri Chavali <64172984+anusrichavali@users.noreply.github.com>
2024-07-24 22:15:29 -0700

    Time: 7 ms (97.56%) | Memory: 11.8 MB (54.30%) - LeetSync

---
+++ 392-is-subsequence/is-subsequence.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def isSubsequence(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """
+        if len(s) > len(t):
+            return False
+        s_ptr = 0
+        t_ptr = 0
+        while s_ptr < len(s) and t_ptr < len(t):
+            while t_ptr < len(t) and t[t_ptr] != s[s_ptr]:
+                t_ptr += 1
+            if t_ptr < len(t) and t[t_ptr] == s[s_ptr]:
+                print(s[s_ptr])
+                print(t[t_ptr])
+                s_ptr += 1
+                t_ptr += 1
+        return s_ptr == len(s)
+        
